09d27cb14dcdc53eac460b664379b410a55a02b0
Rick Haffer <rickhaffer1992@gmail.com>
2021-03-06 12:42:08 -0700

    Create_New_Appointment Start time box converted and updated to EST

--- src/view_controller/nav_view_controller/appt_view_controller/Create_New_Appointment.java
+++ src/view_controller/nav_view_controller/appt_view_controller/Create_New_Appointment.java
@@ -11,8 +11,15 @@ import model.Customer;
 import view_controller.BaseController;
 
 import java.sql.SQLException;
+import java.time.*;
+import java.util.TimeZone;
 
 public class Create_New_Appointment extends BaseController {
+    int APPOINTMENT_LENGTH = 30;
+    //Business hours are 8AM to 10PM EST
+    int BUSINESS_OPEN = 8;
+    int BUSINESS_CLOSED = 22;
+
 
     @FXML
     ComboBox<Customer> customerComboBox;
@@ -39,10 +46,10 @@ public class Create_New_Appointment extends BaseController {
     DatePicker dateDatePicker;
 
     @FXML
-    ComboBox<String> startComboBox;
+    ComboBox<LocalTime> startComboBox;
 
     @FXML
-    ComboBox<String> endComboBox;
+    ComboBox<LocalTime> endComboBox;
 
     @FXML
     Button createApptButton;
@@ -50,11 +57,55 @@ public class Create_New_Appointment extends BaseController {
     @FXML
     private void initialize() throws SQLException {
         populateCustomerComboBox();
+        populateStartTimeBox();
     }
 
     private void populateCustomerComboBox() throws SQLException {
         CustomerDAO dao = new CustomerDAO();
         ObservableList<Customer> customers = dao.getAll(CONN);
         customerComboBox.setItems(customers);
+    }
+
+    private ZonedDateTime convertEstToLocalTime(LocalDateTime date){
+        //Converts a LocalDateTime to EST and returns that time in Local
+        ZoneId estZoneId = ZoneId.of("America/New_York");
+        ZoneId localZoneId = ZoneId.of(TimeZone.getDefault().getID());
+        ZonedDateTime estZDT = ZonedDateTime.of(date, estZoneId);
+        // Returns a ZDT object in the local time zone
+        return estZDT.withZoneSameInstant(localZoneId);
+    }
+
+    private void populateStartTimeBox(){
+        //Only gives available appointment times for business open hours
+        LocalDate currentDate = LocalDate.now();
+        LocalTime startTime = LocalTime.of(BUSINESS_OPEN, 0);
+        //Turns current date and business open hours to LDT
+        LocalDateTime startDateTime = LocalDateTime.of(currentDate, startTime);
+        //Converts that time from EST to Local
+        ZonedDateTime estStartDateTime = convertEstToLocalTime(startDateTime);
+        LocalTime start = estStartDateTime.toLocalTime();
+
+        LocalTime endTime = LocalTime.of(BUSINESS_CLOSED, 0);
+        //Turns current date and business closed hours to LDT
+        LocalDateTime endDateTime = LocalDateTime.of(currentDate, endTime);
+        //Converts that time from EST to Local
+        ZonedDateTime estEndDateTime = convertEstToLocalTime(endDateTime);
+        LocalTime end = estEndDateTime.toLocalTime();
+
+        while(start.isBefore(end.plusSeconds(1))){
+            startComboBox.getItems().add(start);
+            start = start.plusMinutes(APPOINTMENT_LENGTH);
+            //TODO check DB to see if times overlap
+        }
+    }
+
+    private void populateEndTimeBox(){
+        //TODO have this populate once start time is selected and then add all the conversions
+        LocalTime start = startComboBox.getValue();
+        LocalTime end = LocalTime.of(12, 0);
+        while(start.isBefore(end.plusSeconds(1))){
+            endComboBox.getItems().add(start);
+            start = start.plusMinutes(APPOINTMENT_LENGTH);
+        }
     }
 }
